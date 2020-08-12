70ec97338d3d3263cdd798e6e9d295a6258a0459
phmiranda <pehhagah.1607@gmail.com>
2020-08-12 12:16:00 -0300

    [add] - criação da entidade de exemplo Foo para testes do mapeamento com o hibernate.

---
+++ src/main/java/br/com/novatec/entity/Foo.java
@@ -0,0 +1,12 @@
+/**
+ * User: phmiranda
+ * Project: hibernate
+ * Description: classe de representação da entidade Foo para mapeamento no hibernate.
+ * Date: 12/08/2020
+ */
+
+package br.com.novatec.entity;
+
+public class Foo {
+    // comment
+}
